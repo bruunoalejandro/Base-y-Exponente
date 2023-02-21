8afd849f836dea0f0195888b8f83127f5fae068f
Bruno Fernandes <125995286+bruunoalejandro@users.noreply.github.com>
2023-02-21 14:41:49 -0300

    Add files via upload

---
+++ base_exponenteF.py
@@ -0,0 +1,5 @@
+import math
+x = int(input("Ingrese el numero de la base: "))
+y = int(input("Ingrese el numero del exponente: "))
+res = math.pow(x, y)
+print("El resuldado de",x,"elevado a",y,"es:",res)
